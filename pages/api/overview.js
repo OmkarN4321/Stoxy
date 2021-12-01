90b8ce8b59363162c404ada4bc743ddbc00edb5a
Largonarco <snarayankars62@gmail.com>
2021-12-01 12:12:25 +0530

    Addes status codes

--- pages/api/overview.js
+++ pages/api/overview.js
@@ -7,6 +7,10 @@ export default async function handler(req, res){
           );
         const data = await res.json();
 
-        res.json(data)
+        if(data){
+            res.status(200).json(data)
+        }else{
+            res.status(404).json({error: "Unable to fetch data"})
+        }
     }
 }
--- pages/api/stock.js
+++ pages/api/stock.js
@@ -8,6 +8,10 @@ export default async function handler(req, res){
           );
         const data = await values.json();
 
-        res.json(data)
+        if(data){
+            res.status(200).json(data)
+        }else{
+            res.status(404).json({error: "Unable to fetch data"})
+        }
     }
 }
